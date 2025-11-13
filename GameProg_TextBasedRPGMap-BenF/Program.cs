2c1d9e77ce3ce7233bc1f2b59dc89423e7b83631
Frosti-Dev <bennyfranken3@gmail.com>
2025-11-12 20:06:47 -0400

    Map Scaling and Border Done

--- GameProg_TextBasedRPGMap-BenF/Program.cs
+++ GameProg_TextBasedRPGMap-BenF/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,31 +37,124 @@ namespace GameProg_TextBasedRPGMap_BenF
 
         static void DisplayMap()
         {
+            Console.WriteLine("Map legend: ");
+            Console.WriteLine("^ = mountain");
+            Console.WriteLine("` = grass");
+            Console.WriteLine("~ = water");
+            Console.WriteLine("* = trees");
+
+            //border top
+            Console.Write("+");
+            for (int k = 0; k < map.GetLength(0) * 2; k++)
+            {
+                Console.Write('-');
+            }
+            Console.Write('+');
+            //Console.WriteLine(" ");
+            
             for (int i = 0; i < map.GetLength(0); i++)
             {
                 Console.WriteLine(" ");
+                Console.Write('|');
                 for (int j = 0; j < map.GetLength(0); j++)
                 {
                     Console.Write(map[i, j] + " ");
                 }
+                Console.Write('|');
             }
+
+
+            //bottom border
+            Console.WriteLine(" ");
+            Console.Write("+");
+            for (int f = 0; f < map.GetLength(0) * 2; f++)
+            {
+                Console.Write('-');
+            }
+            Console.Write('+');
+            Console.WriteLine(" ");
         }
 
         static void DisplayMap(int scale)
         {
+            Console.WriteLine("Map legend: ");
+            Console.WriteLine("^ = mountain");
+            Console.WriteLine("` = grass");
+            Console.WriteLine("~ = water");
+            Console.WriteLine("* = trees");
+
+
+            //border top
+            Console.Write("+");
+            for(int k = 0;  k < map.GetLength(0) * scale * 2; k++)
+            {
+                Console.Write('-');
+            }
+            Console.Write('+');
+            Console.WriteLine(" ");
+
+            //print map
             for (int i = 0; i < map.GetLength(0); i++)
             {
-                Console.WriteLine(" ");
-                for (int j = 0; j < map.GetLength(0); j++)
+                    
+                int row = 0;
+                while (row < scale)
                 {
-                    Console.Write(map[i , j] + " ");
+                    Console.Write('|');
+                    for (int j = 0; j < map.GetLength(0); j++)
+                    {
+                        int timer = 0;
+                        while (timer < scale)
+                        {
+                            Console.Write(map[i, j] + " ");
+                            timer++;
+                        }
+                    }
+                    Console.Write('|');
+                    Console.WriteLine(" ");
+                    row++;
                 }
             }
+
+            //bottom border
+            Console.Write("+");
+            for (int f = 0; f < map.GetLength(0) * scale * 2; f++)
+            {
+                Console.Write('-');
+            }
+            Console.Write('+');
+            Console.WriteLine(" ");
         }
 
         static void Main(string[] args)
-        {
-           DisplayMap();
+        { 
+            Console.WriteLine("Display 1");
+            Console.WriteLine("  ");
+
+
+            DisplayMap();
+
+            Console.WriteLine("  ");
+            Console.WriteLine("  ");
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("  ");
+            Console.WriteLine("  ");
+
+            Console.WriteLine("Display 2");
+            Console.WriteLine("  ");
+
+            DisplayMap(2);
+
+            Console.WriteLine("  ");
+            Console.WriteLine("  ");
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("  ");
+            Console.WriteLine("  ");
+
+            Console.WriteLine("Display 3");
+            Console.WriteLine("  ");
+
+            DisplayMap(3);
         }
     }
 }
